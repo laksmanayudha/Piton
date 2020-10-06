ce638f2a1fec629d2b18223e8080eeb1c9e80edb
laksmanayudha <laksmanayudha22@gmail.com>
2020-10-06 09:51:37 +0800

    menambahkan data L4 dan L5

--- tbo/dfa.py
+++ tbo/dfa.py
@@ -1,6 +1,6 @@
-def dfa (state, str, finalState, transitions):
+def dfa_result (state, string, finalState, transitions):
     
-    hasil = deltaTopi(state, str, transitions)
+    hasil = deltaTopi(state, string, transitions)
 
     # cek apabila state hasil termasuk ke dalam final state
     if hasil in finalState:
@@ -15,18 +15,18 @@ def delta (state, a, transitions):
     # kembalikan nilai state  dari transition yang sesuai
     return transitions[state][a]
 
-def deltaTopi (state, str, transitions):
+def deltaTopi (state, string, transitions):
 
     # cek epsilon
     # lakukan delta topi hingga x sama dengan epsilon, jika belum lakukan rekursif
     # fungsi deltaTopi() mengembalikan state yang dipilih oleh fungsi delta()
     # kemudian update nilai dari state,  yang selanjutnya dijadikan parameter untuk menentukan state selanjutnya oleh fungsi delta()
-    if str != '':
+    if string != '':
         # memecah string
         # a = char terakhir
         # x = sisa string
-        x = str[:-1]
-        a = str[-1]
+        x = string[:-1]
+        a = string[-1]
         state = deltaTopi(state, x, transitions)
     else:
         return state
@@ -35,8 +35,8 @@ def deltaTopi (state, str, transitions):
     # lalu kembalikan nilai state tersebut ke fungsi yang memanggilnya
     return delta(state, a, transitions)
 
-def isStringValid(alphabet, str):
-    for char in str:
+def isStringValid(alphabet, string):
+    for char in string:
         if char not in alphabet:
             return False
     
@@ -45,110 +45,10 @@ def isStringValid(alphabet, str):
 def input_string():
     alphabet = ['1', '0']
     while(True):
+        print('\n')
         string = input('ketik string (Alphabet = {1, 0}) :')
         print('String anda adalah ', string)
         if isStringValid(alphabet, string):
             return string
         else:
             print('String yang anda masukan tidak sesuai dengan alphabet')
-
-moduls = [
-    {
-        'modulName':'L1',
-        'description':'L1 adalah himpunan semua string yang mengandung substring 101',
-        'initialState' : 'q0',
-        'finalState' : ['q3'],
-        'allState' : ['q0', 'q1', 'q2', 'q3'],
-        'transitions':{
-            'q0':{
-                '0':'q0',
-                '1':'q1'
-            },
-            'q1':{
-                '0':'q2',
-                '1':'q1'
-            },
-            'q2':{
-                '0':'q0',
-                '1':'q3'
-            },
-            'q3':{
-                '0':'q3',
-                '1':'q3'
-            }
-        }
-    },
-    {
-        'modulName':'L2',
-        'description':'L2 adalah himpunan semua string yang mengandung prefiks 101',
-        'initialState' : 'q0',
-        'finalState' : ['q4'],
-        'allState' : ['q0', 'q1', 'q2', 'q3', 'q4'],
-        'transitions':{
-            'q0':{
-                '0':'q3',
-                '1':'q1'
-            },
-            'q1':{
-                '0':'q2',
-                '1':'q3'
-            },
-            'q2':{
-                '0':'q3',
-                '1':'q4'
-            },
-            'q3':{
-                '0':'q3',
-                '1':'q3'
-            },
-            'q4':{
-                '0':'q4',
-                '1':'q4'
-            }
-        }
-    },
-    {
-        'modulName':'Input Lagi',
-        'description':'input string baru!'
-    },
-    {
-        'modulName':'Selesai',
-        'description':'Program telah berakhir..'
-    }
-    
-]
-
-
-# program utama
-lanjut = True
-string = input_string()
-#menu
-while(lanjut):
-    #print menu
-    print('\nPilih Modul :')
-    for i, v in enumerate(moduls):
-        print(i+1, v['modulName'], sep='. ')
-        
-    #pilih menu
-    menu = int(input('Pilih Menu : '))
-    
-    if menu <= 0 or menu > len(moduls):
-        print('Menu tidak tersedia. Mohon input menu kembali..')
-    elif moduls[menu-1]['modulName'] == 'Input Lagi':
-        string = input_string()
-    elif moduls[menu-1]['modulName'] == 'Selesai':
-        print(moduls[menu-1]['description'])
-        lanjut = False
-    else:
-        #print deskripsi modul
-        print(moduls[menu-1]['description'])
-        
-        # memulai dfa
-        initialState = moduls[menu-1]['initialState']
-        finalState = moduls[menu-1]['finalState']
-        transitions = moduls[menu-1]['transitions']
-        isAccepted, hasil = dfa(initialState, string, finalState, transitions)
-        if isAccepted:
-            print('string diterima dengan final state adalah ',hasil)
-        else:
-            print('string tidak diterima dengan state akhir adalah',hasil)
---
+++ tbo/main.py
@@ -0,0 +1,140 @@
+from dfa import dfa_result, isStringValid, input_string
+
+data = [
+    {
+        'name':'L1',
+        'description':'L1 adalah himpunan semua string yang mengandung substring 101',
+        'initialState' : 'q0',
+        'finalState' : ['q3'],
+        'allState' : ['q0', 'q1', 'q2', 'q3'],
+        'transitions':{
+            'q0':{
+                '0':'q0',
+                '1':'q1'
+            },
+            'q1':{
+                '0':'q2',
+                '1':'q1'
+            },
+            'q2':{
+                '0':'q0',
+                '1':'q3'
+            },
+            'q3':{
+                '0':'q3',
+                '1':'q3'
+            }
+        }
+    },
+    {
+        'name':'L2',
+        'description':'L2 adalah himpunan semua string yang mengandung prefiks 101',
+        'initialState' : 'q0',
+        'finalState' : ['q4'],
+        'allState' : ['q0', 'q1', 'q2', 'q3', 'q4'],
+        'transitions':{
+            'q0':{
+                '0':'q3',
+                '1':'q1'
+            },
+            'q1':{
+                '0':'q2',
+                '1':'q3'
+            },
+            'q2':{
+                '0':'q3',
+                '1':'q4'
+            },
+            'q3':{
+                '0':'q3',
+                '1':'q3'
+            },
+            'q4':{
+                '0':'q4',
+                '1':'q4'
+            }
+        }
+    },
+    {
+        'name':'L4',
+        'description':'L4 adalah himpunan semua string yang mengandung jumlah simbol 0 genap',
+        'initialState' : 'q0',
+        'finalState' : ['q0'],
+        'allState' : ['q0', 'q1'],
+        'transitions':{
+            'q0':{
+                '0':'q1',
+                '1':'q0'
+            },
+            'q1':{
+                '0':'q0',
+                '1':'q1'
+            }
+        }
+    },
+    {
+        'name':'L5',
+        'description':'L5 adalah himpunan semua string yang mengandung jumlah simbol 1 ganjil',
+        'initialState' : 'q0',
+        'finalState' : ['q1'],
+        'allState' : ['q0', 'q1'],
+        'transitions':{
+            'q0':{
+                '0':'q0',
+                '1':'q1'
+            },
+            'q1':{
+                '0':'q1',
+                '1':'q0'
+            }
+        }
+    },
+    {
+        'name':'Input Lagi',
+        'description':'input string baru'
+    },
+    {
+        'name':'Selesai',
+        'description':'Selesai'
+    }
+    
+]
+
+
+# program utama
+lanjut = True
+string = input_string()
+
+#menu
+while(lanjut):
+    #print menu
+    print('\nPilih Modul :')
+    for i, v in enumerate(data):
+        print(i+1, v['description'], sep='. ')
+        
+    #pilih menu
+    menu = int(input('Pilih Menu : '))
+    
+    if menu > 0 and menu <= len(data):
+        if data[menu-1]['name'] == 'Input Lagi':
+            string = input_string()
+        elif data[menu-1]['name'] == 'Selesai':
+            print(data[menu-1]['description'])
+            lanjut = False
+        else:
+            #print deskripsi modul
+            print('\n')
+            print(data[menu-1]['description'])
+            
+            # memulai dfa
+            initialState = data[menu-1]['initialState']
+            finalState = data[menu-1]['finalState']
+            transitions = data[menu-1]['transitions']
+            isAccepted, hasil = dfa_result(initialState, string, finalState, transitions)
+            if isAccepted:
+                print('string diterima dengan final state adalah ',hasil)
+            else:
+                print('string tidak diterima dengan state akhir adalah',hasil)
+    else:
+        print('Menu tidak tersedia. Mohon input menu kembali..')
+        
